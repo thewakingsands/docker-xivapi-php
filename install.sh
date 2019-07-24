d1e262f685f87d10215a3bd60b23247920c4d519
oott123 <git@public.oott123.com>
2019-07-24 10:58:51 +0800

    install yarn using package manager

--- install.sh
+++ install.sh
@@ -86,7 +86,12 @@ composer install --no-scripts
 # nodejs and yarn
 curl -sL https://deb.nodesource.com/setup_12.x | bash -
 apt-get install -y nodejs
-curl -o- -L https://yarnpkg.com/install.sh | bash
+
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+apt-get update -y
+apt-get install -y yarn
+
 # encore
 cd /vagrant
 yarn

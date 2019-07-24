7327355eddad0ab7bfab715429fb26b0bafc8129
oott123 <git@public.oott123.com>
2019-07-24 11:13:51 +0800

    no sudo

--- install.sh
+++ install.sh
@@ -87,8 +87,8 @@ composer install --no-scripts
 curl -sL https://deb.nodesource.com/setup_12.x | bash -
 apt-get install -y nodejs
 
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 apt-get update -y
 apt-get install -y yarn
 
